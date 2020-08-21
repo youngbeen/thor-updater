3e3a4950fe6e8dd461cb8ed56d789a1635292374
xhyang2 <xhyang2@chinaums.com>
2020-08-21 16:58:21 +0800

    perf: styling the console log

--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@
 
 const fs = require('fs-extra')
 const path = require('path')
+const style = require('chalk')
+
+const info = style.cyan.bold
+const success = style.green.bold
+const warning = style.yellow.bold
+const error = style.red.bold
+// const tip = style.gray
 
 let isThorProject = true // 是否是Thor生成的工程
 
@@ -10,20 +17,20 @@ let pathInfo = path.parse(currentPath)
 
 const updateBizPage = () => {
   fs.copy('node_modules/thor-bizpage-update/assets/views/', 'src/views/').then(() => {
-    console.log('可配置化业务支持文件已更新')
+    console.log(success('可配置化业务支持文件已更新'))
   }).catch((err) => {
-    console.error(err)
+    console.error(error(err))
   })
 }
 
 const addFeature = () => {
   Promise.all([addConfig(), addBizPage()]).then(() => {
     // 全部完成
-    console.log(`可配置化业务已全部添加完成！`)
-    console.log(`配置文件路径：src/models/SystemConfig.js`)
-    console.warn(`请注意：可配置化业务的路由配置需手动配置`)
+    console.log(success(`可配置化业务已全部添加完成！`))
+    console.log(`配置文件路径：${info('src/models/SystemConfig.js')}`)
+    console.warn(warning(`请注意：可配置化业务的路由配置需手动配置`))
   }).catch(err => {
-    console.error(err)
+    console.error(error(err))
   })
 }
 
@@ -33,10 +40,10 @@ const addConfig = () => new Promise((resolve, reject) => {
     return fs.copy('node_modules/thor-bizpage-update/assets/models/', 'src/models/')
   }).then(() => {
     // 拷贝成功
-    console.log('配置文件已添加')
+    console.log(success('配置文件已添加'))
     resolve()
   }).catch(err => {
-    console.error(err)
+    console.error(error(err))
     reject(err)
   })
 })
@@ -47,10 +54,10 @@ const addBizPage = () => new Promise((resolve, reject) => {
     return fs.copy('node_modules/thor-bizpage-update/assets/views/', 'src/views/')
   }).then(() => {
     // 拷贝成功
-    console.log('可配置化业务已添加')
+    console.log(success('可配置化业务已添加'))
     resolve()
   }).catch(err => {
-    console.error(err)
+    console.error(error(err))
     reject(err)
   })
 })
@@ -58,14 +65,14 @@ const addBizPage = () => new Promise((resolve, reject) => {
 fs.exists('src/models/SystemConfig.js').then((exist) => {
   // 判断工程是否存在配置文件
   isThorProject = exist
-  console.log(`当前工程：${pathInfo.name}  状态：${isThorProject ? 'Thor工程' : '非Thor历史工程'}  路径：${currentPath}`)
+  console.log(`当前工程：${info(pathInfo.name)}  状态：${isThorProject ? info('Thor工程') : warning('非Thor历史工程')}  路径：${info(currentPath)}`)
   if (isThorProject) {
     // thor工程，只更新可配置化业务文件
-    console.log(`开始更新工程 ${pathInfo.name} 的可配置化业务`)
+    console.log(info(`开始更新工程 ${warning(pathInfo.name)} 的可配置化业务`))
     updateBizPage()
   } else {
     // 老工程，需要添加配置文件+可配置化业务文件
-    console.log(`开始为工程 ${pathInfo.name} 添加可配置化业务支持`)
+    console.log(info(`开始为工程 ${warning(pathInfo.name)} 添加可配置化业务支持`))
     addFeature()
   }
 }).catch(err => {
